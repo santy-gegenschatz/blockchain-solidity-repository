cec05782f3f7bc27e538f096e57fa00f079f5b68
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-04-09 19:36:59 -0300

    Improved Calculator Functions

--- calculator.sol
+++ calculator.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Calculator {
+import "./Owner.sol";
+
+contract Calculator is Owner {
     uint calculationsQuantity;
     uint lastResult;
+    uint [] historicCalculations;
 
     mapping(address => uint []) personalResults;
 
@@ -19,5 +22,35 @@ contract Calculator {
         calculationsQuantity++;
         personalResults[msg.sender].push(lastResult);
         return lastResult;
+    }
+
+    function multiply(uint num1, uint num2) public returns (uint) {
+        lastResult = num1 * num2;
+        calculationsQuantity++;
+        personalResults[msg.sender].push(lastResult);
+        return lastResult;
+    }
+
+    function divide(uint num1, uint num2) public returns (uint) {
+        lastResult = num1 / num2;
+        calculationsQuantity++;
+        personalResults[msg.sender].push(lastResult);
+        return lastResult;
+    }
+
+    function showHistoricalAverage() public view isOwner returns (uint) {
+        uint average;
+        for (uint i = 0; i < historicCalculations.length; i++) {
+            average += historicCalculations[i];
+        }
+        return average/historicCalculations.length;
+    }
+
+    function showPersonalHistoricalAverage() public view returns (uint) {
+        uint average;
+        for (uint256 i = 0; i < personalResults[msg.sender].length; i++) {
+            average += personalResults[msg.sender][i];
+        }
+        return average / personalResults[msg.sender].length;
     }
 }
